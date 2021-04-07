a4fc124012794c3a4b68f003767285b8479f4b08
minkyungyeee <minkfree94@gmail.com>
2021-04-08 00:14:49 +0900

    Update 초기작업

---
+++ js/pofol.js
@@ -0,0 +1,44 @@
+;(function($){
+    var pofol = {
+        init:function(){
+            var that = this;
+                that.headerFn();
+                that.section1Fn();
+                that.section2Fn();
+                that.section3Fn();
+                that.section4Fn();
+                that.section5Fn();
+                that.section6Fn();
+                that.section7Fn();
+                that.footerFn();
+        },
+        headerFn:function(){
+            
+        },
+        section1Fn:function(){
+
+        },
+        section2Fn:function(){
+
+        },
+        section3Fn:function(){
+
+        },
+        section4Fn:function(){
+
+        },
+        section5Fn:function(){
+
+        },
+        section6Fn:function(){
+
+        },
+        section7Fn:function(){
+
+        },
+        footerFn:function(){
+
+        }
+    }
+    pofol.init();
+})(jQuery);
