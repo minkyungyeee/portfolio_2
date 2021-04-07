2237c4ad711cc78830a172063a8af5fbfac627a6
minkyungyeee <minkfree94@gmail.com>
2021-04-07 10:39:06 +0900

    Update am

---
+++ js/jump.js
@@ -0,0 +1,17 @@
+function goUrl(z){
+    switch(z){
+        case 'familySite':
+            location.href = '';
+            break
+
+        case 'insta':
+            window.open('https://www.instagram.com');
+            break
+
+        case 'demo':
+            location.href = 'https://lithohtml.themezaa.com/home-furniture-shop.html';
+            break
+        default:
+            alert();
+    }
+}
